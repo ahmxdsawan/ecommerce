b46d1f3dc5376ec9f7f3609c972b745e12d397ac
Ahmad Sawan <ahmadsawan@Ahmads-MacBook-Pro.local>
2021-08-11 15:02:16 -0400

    Initial Commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 
@@ -11,23 +13,27 @@ function HomeScreen() {
     const { error, loading, products } = productList
 
     useEffect(() => {
-        dispatch(listProducts())      
+        dispatch(listProducts())    
 
-    
     }, [])
-
-
+ 
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xL={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </div>
+            {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    : 
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+            }
+
+
+    </div>
     )
 }
 
